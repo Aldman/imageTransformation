64b93cf10a99ab53ee7e90884b0ec0e0b7bf1ea1
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-07-15 16:22:12 +0300

    Complete convert to grayscale

--- GrayscaleTask.cs
+++ GrayscaleTask.cs
@@ -1,26 +1,77 @@
-﻿namespace Recognizer
-{
-	public static class GrayscaleTask
-	{
-		/* 
-		 * Переведите изображение в серую гамму.
-		 * 
-		 * original[x, y] - массив пикселей с координатами x, y. 
-		 * Каждый канал R,G,B лежит в диапазоне от 0 до 255.
-		 * 
-		 * Получившийся массив должен иметь те же размеры, 
-		 * grayscale[x, y] - яркость пикселя (x,y) в диапазоне от 0.0 до 1.0
-		 *
-		 * Используйте формулу:
-		 * Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
-		 * 
-		 * Почему формула именно такая — читайте в википедии 
-		 * http://ru.wikipedia.org/wiki/Оттенки_серого
-		 */
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System;
 
-		public static double[,] ToGrayscale(Pixel[,] original)
-		{
-			return new double[original.GetLength(0), original.GetLength(1)];
-		}
-	}
+namespace Recognizer
+{
+    [TestFixture]
+    public class GrayscaleTaskTests
+    {
+        public static IEnumerable<TestCaseData> Tests
+        {
+            get
+            {
+                var testImage1 = new Pixel[,] { { new Pixel(10, 10, 10) } };
+                var expectedResult = new double[,] { { GrayscaleTask.GetGrayBright(new Pixel(10, 10, 10)) } };
+                yield return new TestCaseData(testImage1, expectedResult).SetName("Image 3x3");
+                testImage1 = new Pixel[,]
+                {
+                    {new Pixel(164, 97, 211), new Pixel(174, 0, 49), new Pixel(234, 35, 28), new Pixel(134, 204, 87), new Pixel(234, 54, 178) },
+                    {new Pixel(35, 218, 223), new Pixel(255, 97, 130), new Pixel(89, 165, 38), new Pixel(135, 187, 22), new Pixel(116, 86, 251) },
+                    {new Pixel(99, 7, 160), new Pixel(240, 122, 136), new Pixel(245, 166, 222), new Pixel(38, 214, 132), new Pixel(184, 43, 85) }
+                };
+                expectedResult = new double[,] 
+                {
+                    { GrayscaleTask.GetGrayBright(new Pixel(164, 97, 211)), GrayscaleTask.GetGrayBright(new Pixel(174, 0, 49)),
+                    GrayscaleTask.GetGrayBright(new Pixel(234, 35, 28)), GrayscaleTask.GetGrayBright(new Pixel(134, 204, 87)),
+                    GrayscaleTask.GetGrayBright(new Pixel(234, 54, 178))},
+
+                    {
+                        GrayscaleTask.GetGrayBright(new Pixel(35, 218, 223)), GrayscaleTask.GetGrayBright(new Pixel(255, 97, 130)),
+                    GrayscaleTask.GetGrayBright(new Pixel(89, 165, 38)), GrayscaleTask.GetGrayBright(new Pixel(135, 187, 22)),
+                    GrayscaleTask.GetGrayBright(new Pixel(116, 86, 251))
+                    },
+
+                    {
+                        GrayscaleTask.GetGrayBright(new Pixel(99, 7, 160)), GrayscaleTask.GetGrayBright(new Pixel(240, 122, 136)),
+                    GrayscaleTask.GetGrayBright(new Pixel(245, 166, 222)), GrayscaleTask.GetGrayBright(new Pixel(38, 214, 132)),
+                    GrayscaleTask.GetGrayBright(new Pixel(184, 43, 85))
+                    }
+                };
+                yield return new TestCaseData(testImage1, expectedResult).SetName("Image 3x5");
+            }
+        }
+
+        [TestCaseSource("Tests")]
+        public void ToGrayscaleTests(Pixel[,]image, double [,] expeсtedGrayscale)
+        {
+            var result = GrayscaleTask.ToGrayscale(image);
+            Assert.AreEqual(expeсtedGrayscale, result);
+        }
+    }
+
+    public static class GrayscaleTask
+    {
+        public static double[,] ToGrayscale(Pixel[,] original)
+        {
+            var widthOfArray = original.GetLength(0);
+            var heightOfArray = original.GetLength(1);
+            var grayscale = new double[widthOfArray, heightOfArray];
+            for (int i = 0; i < widthOfArray; i++)
+                for (int j = 0; j < heightOfArray; j++)
+                    grayscale[i, j] = GetGrayBright(original[i, j]);
+                
+            return grayscale;
+        }
+
+        public static double GetGrayBright (Pixel color)
+        {
+            var brightPixel = (0.299 * color.R
+                        + 0.587 * color.G
+                        + 0.114 * color.B) / 255;
+            if (brightPixel > 1)
+                brightPixel = 1;
+            return brightPixel;
+        }
+    }
 }
