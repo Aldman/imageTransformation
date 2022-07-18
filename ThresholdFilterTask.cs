bc2f3bb877b4b2046d589963e47b7634b4684523
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-07-18 16:51:20 +0300

    First try to threshold filter

--- ThresholdFilterTask.cs
+++ ThresholdFilterTask.cs
@@ -1,10 +1,41 @@
-﻿namespace Recognizer
+﻿using System.Collections.Generic;
+using System;
+
+namespace Recognizer
 {
 	public static class ThresholdFilterTask
 	{
 		public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
 		{
-			return new double[original.GetLength(0), original.GetLength(1)];
+            var thresholdColor = GetThreshholdColor
+                (original, whitePixelsFraction);
+            
+            
+            return new double[original.GetLength(0), original.GetLength(1)];
 		}
+
+        public static double[,] GetBWColors
+            (double[,] original, double thresholdColor)
+        {
+            //TODO: Копировать оригинальный массив в новый,
+            //  заменить все цвета >= пороговому на белый, вернуть
+
+            var bwResult = new double[original.GetLength(0),
+                original.GetLength(1)];
+            Array.Copy(original, bwResult, original.Length);
+            return bwResult;
+        }
+
+
+        public static double GetThreshholdColor
+            (double[,] original, double whitePixelsFraction)
+        {
+            var whitePixelsCountMin = (int)(original.Length
+                * whitePixelsFraction);
+            var originalList = new List<double>();
+            originalList.AddRange(originalList);
+            originalList.Sort();
+            return originalList[whitePixelsCountMin];            
+        }
 	}
 }
---
+++ ThresholderFilterTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Recognizer
+{
+    public class ThresholderFilterTests
+    {
+        public static IEnumerable<TestCaseData> GetBWColorsCaseData
+        {
+            get
+            {
+                var original = new double[,]
+                {
+                    {0, 1, 2 },
+                    {3, 4, 5 }
+                };
+                double thresholdColor = 1.0;
+                var expectedResult = new double[,]
+                {
+                    {0, 1, 2 },
+                    {3, 4, 5 }
+                };
+                yield return new TestCaseData(
+                    original, thresholdColor, expectedResult)
+                    .SetName("First method check");
+            }
+        }
+
+        [TestCaseSource("GetBWColorsCaseData")]
+        public void GetBWColorsTests (double[,] original, 
+            double thresholdColor, double[,] expected)
+        {
+            var actualResult = ThresholdFilterTask
+                .GetBWColors(original, thresholdColor);
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
