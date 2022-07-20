af97e8550ae71132bf18bdbe93fa7d043e2799c7
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-07-20 16:45:02 +0300

    First try to refactoring sobel filter

--- SobelFilterTask.cs
+++ SobelFilterTask.cs
@@ -9,20 +9,102 @@ namespace Recognizer
             var width = g.GetLength(0);
             var height = g.GetLength(1);
             var result = new double[width, height];
+            var sy = GetTranspositionArray(sx);
+            var sxSide = sx.GetLength(0);
             for (int x = 1; x < width - 1; x++)
                 for (int y = 1; y < height - 1; y++)
                 {
-                    // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной транспонированием из неё sy на окрестность точки (x, y)
-                    // Такая операция ещё называется свёрткой (Сonvolution)
-                    var gx = 
-                        -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1] 
+                    //TODO: взять массив в окрестности точки [x,y], равный массиву фильтра
+                    //  Новое значение точки будет равно сумме поэлементных произведению g и фильтра
+                    // для отдельно для sx и sy. 
+                    //  Затем по нижней формуле определяется итоговое новое значение пикселя
+                    var subArray = GetSquareSubArray(g, sxSide, x, y);
+                    var gx =
+                        -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1]
                         + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                    var gy = 
-                        -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1] 
+                    var gy =
+                        -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1]
                         + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
                     result[x, y] = Math.Sqrt(gx * gx + gy * gy);
                 }
             return result;
+        }
+
+        public static double CalculateElByElSquareArrayMulWithAdd
+            (double[,] array1, double[,] array2)
+        {
+            if((array1.GetLength(0) != array2.GetLength(0))
+                || (array1.GetLength(1) != array2.GetLength(1)))
+                throw new Exception("Размеры массивов не одинаковы");
+
+            var width = array1.GetLength(0);
+            var height = array1.GetLength(1);
+            var elByElMulledArray = new double[width, height];
+            for (int heightPosition = 0; heightPosition < height; heightPosition++)
+                for (int widthPosition = 0; widthPosition < width; widthPosition++)
+                    elByElMulledArray[widthPosition, heightPosition]
+                        = array1[widthPosition, heightPosition]
+                        * array2[widthPosition, heightPosition];
+            double numberForReturning = 0;
+            foreach (var number in elByElMulledArray)
+                numberForReturning += number;
+            return numberForReturning;
+        }
+
+        public static double[,] GetSquareSubArray
+            (double[,] originalArray, int subArraySide,
+            int x, int y)
+        {
+            var width = originalArray.GetLength(0);
+            var height = originalArray.GetLength(1);
+            var deltaSubArraySide = (int)(subArraySide / 2);
+            if ((x + deltaSubArraySide >= width)
+                || (x - deltaSubArraySide < 0)
+                || (y + deltaSubArraySide >= height)
+                || (y - deltaSubArraySide < 0))
+                throw new Exception("Оригинальный массив меньше подмассива");
+
+            var subArray = new double[subArraySide, subArraySide];
+            int xSub = 0;
+            for (int xOriginal = x - deltaSubArraySide; xOriginal <= x + deltaSubArraySide; xOriginal++)
+            {
+                int ySub = 0;
+                for (int yOriginal = y - deltaSubArraySide; yOriginal <= y + deltaSubArraySide; yOriginal++)
+                {
+                    subArray[xSub, ySub] = originalArray[xOriginal, yOriginal];
+                    ySub++;
+                }
+                xSub++;
+            }
+            return subArray;
+        }
+
+        public static double[,] Mul2DimensionalArrayToNumber(
+            double[,] arrayToMul, double numberToMul)
+        {
+            var width = arrayToMul.GetLength(0);
+            var height = arrayToMul.GetLength(1);
+            var arrayToReturn = new double[width, height];
+            for (int heightPosition = 0; heightPosition < height; heightPosition++)
+                for (int widthPosition = 0; widthPosition < width; widthPosition++)
+                    arrayToReturn[widthPosition, heightPosition]
+                        = arrayToMul[widthPosition, heightPosition] * numberToMul;
+            return arrayToReturn;
+        }
+
+        public static double[,] GetTranspositionArray(double[,] arrayToTransposition)
+        {
+            var width = arrayToTransposition.GetLength(0);
+            var height = arrayToTransposition.GetLength(1);
+            var arrayToReturn = new double[height, width];
+
+            for (int heightPosition = 0; heightPosition < height; heightPosition++)
+                for (int widthPosition = 0; widthPosition < width; widthPosition++)
+                {
+                    arrayToReturn[heightPosition, widthPosition]
+                        = arrayToTransposition[widthPosition, heightPosition];
+                }
+            return arrayToReturn;
         }
     }
 }
---
+++ SobelFilterTests.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Recognizer
+{
+    [TestFixture]
+    class SobelFilterTests
+    {
+        public static IEnumerable<TestCaseData> GetTranspositionArrayCaseData
+        {
+            get
+            {
+                var arrayToTransposition = new double[,]
+                {
+                    { 7, 3, -12, 0, 34}
+                };
+                var expectedResult = new double[,]
+                {
+                    { 7},
+                    {3 },
+                    {-12 },
+                    {0 },
+                    {34 }
+                };
+                yield return new TestCaseData(arrayToTransposition, expectedResult)
+                    .SetName("Transposition vector");
+
+                arrayToTransposition = new double[,]
+                {
+                    { -1, 0, 2},
+                    {-5, 4, -7 },
+                    {6, -4, -6 }
+                };
+                expectedResult = new double[,]
+                {
+                    { -1, -5, 6},
+                    {0, 4, -4 },
+                    {2, -7, -6 }
+                };
+                yield return new TestCaseData(arrayToTransposition, expectedResult)
+                    .SetName("Transposition 3x3");
+            }
+
+
+        }
+
+        [TestCaseSource("GetTranspositionArrayCaseData")]
+        public void GetTranspositionArrayTests(double[,] arrayToTransposition, double[,] expectedResult)
+        {
+            var actualResult = SobelFilterTask.GetTranspositionArray(arrayToTransposition);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public static IEnumerable<TestCaseData> Mul2DimensionalArrayToNumberCaseData
+        {
+            get
+            {
+                var arrayToMul = new double[,]
+                {
+                    { 7, 3, -12, 0, 34}
+                };
+                double numberToMul = 10;
+                var expectedResult = new double[,]
+                {
+                    { 70, 30, -120, 0, 340}
+                };
+                yield return new TestCaseData
+                    (arrayToMul, numberToMul, expectedResult)
+                    .SetName("Mul vector to 10");
+
+                arrayToMul = new double[,]
+                {
+                    { -1, 0, 2},
+                    {-5, 4, -7 },
+                    {6, -4, -6 }
+                };
+                expectedResult = new double[,]
+                {
+                    { -10, 0, 20},
+                    {-50, 40, -70 },
+                    {60, -40, -60 }
+                };
+                yield return new TestCaseData
+                    (arrayToMul, numberToMul, expectedResult)
+                    .SetName("Mul array 3x3 to 10");
+            }
+        }
+
+        [TestCaseSource("Mul2DimensionalArrayToNumberCaseData")]
+        public void Mul2DimensionalArrayToNumberTests(
+            double[,] arrayToMul, double numberToMul, double[,] expectedResult)
+        {
+            var actualResult = SobelFilterTask
+                .Mul2DimensionalArrayToNumber(arrayToMul, numberToMul);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public static IEnumerable<TestCaseData> GetSquareSubArrayCaseData
+        {
+            get
+            {
+                var originalArray = new double[,]
+                {
+                    { -1, 0, 2},
+                    {-5, 4, -7 },
+                    {6, -4, -6 }
+                };
+                int subArraySide = 1;
+                int x = 1, y = 1;
+                var expectedResult = new double[,]
+                {
+                    { 4}
+                };
+                yield return new TestCaseData
+                    (originalArray, subArraySide, x, y, expectedResult)
+                    .SetName("Get subarray 1x1 from 3x3");
+
+                //originalArray = new double[,]
+                //{
+                //    { -1, 0, 2},
+                //    {-5, 4, -7 },
+                //    {6, -4, -6 }
+                //};
+                subArraySide = 3;
+                x = 1; y = 1;
+                expectedResult = new double[,]
+                {
+                    { -1, 0, 2},
+                    {-5, 4, -7 },
+                    {6, -4, -6 }
+                };
+                yield return new TestCaseData
+                    (originalArray, subArraySide, x, y, expectedResult)
+                    .SetName("Get subarray 3x3 from 3x3");
+
+                originalArray = new double[,]
+                {
+                    { -1, 0, 2, 3, 4, 5, 6, 7, 8},
+                    {-5, 4, -7, 6, 5, 4, 3, 2, 1 },
+                    {6, -4, -6, 5, 4, 3, 2, 1, 0 },
+                    { -1, 0, 2, 3, 4, 5, 6, 7, 8},//
+                    {-5, 4, -7, 6, 5, 4, 3, 2, 1 },
+                    {6, -4, -6, 5, 4, 3, 2, 1, 0 },
+                    { -1, 0, 2, 3, 4, 5, 6, 7, 8},
+                    {-5, 4, -7, 6, 5, 4, 3, 2, 1 },
+                    {6, -4, -6, 5, 4, 3, 2, 1, 0 }
+                };
+                subArraySide = 3;
+                x = 3; y = 3;
+                expectedResult = new double[,]
+                {
+                    { -6, 5, 4},
+                    {  2, 3, 4 },
+                    { -7, 6, 5 }
+                };
+                yield return new TestCaseData
+                    (originalArray, subArraySide, x, y, expectedResult)
+                    .SetName("Get subarray 3x3 from 9x9");
+            }
+        }
+
+        [TestCaseSource("GetSquareSubArrayCaseData")]
+        public void GetSquareSubArrayTests
+            (double[,] originalArray, int subArraySide,
+            int x, int y, double[,] expectedResult)
+        {
+            var actualResult = SobelFilterTask
+                .GetSquareSubArray(originalArray, subArraySide, x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public static IEnumerable<TestCaseData> CalculateElByElSquareArrayMulWithAddCaseData
+        {
+            get
+            {
+                var array1 = new double[,]
+                {
+                    { 1, 2, 3}
+                };
+                var array2 = new double[,]
+                {
+                    { 10, 1, 0}
+                };
+                double expectedNumber = 12;
+                yield return new TestCaseData(array1, array2, expectedNumber)
+                    .SetName("Simple mul with add");
+            }
+        }
+
+        [TestCaseSource("CalculateElByElSquareArrayMulWithAddCaseData")]
+        public void CalculateElByElSquareArrayMulWithAddTests
+            (double[,] array1, double[,] array2, double expectedResult)
+        {
+            var actualResult = SobelFilterTask
+                .CalculateElByElSquareArrayMulWithAdd(array1, array2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
